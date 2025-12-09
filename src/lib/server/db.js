edba8d4b0583dfd95ac02d3ba3174692771607f9
Jakob Rejdemyhr <jakob.rejdemyhr@gmail.com>
2025-12-09 21:05:04 +0100

    change docker compose to work with many, and fix bug in db.js

--- src/lib/server/db.js
+++ src/lib/server/db.js
@@ -1,11 +1,12 @@
 import pg from 'pg'
+import { DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASSWORD } from '$env/static/private'
 
 const pool = new pg.Pool({
-	host: process.env.DB_HOST,
-	port: process.env.DB_PORT,
-	database: process.env.DB_NAME,
-	user: process.env.DB_USER,
-	password: process.env.DB_PASSWORD
+	host: DB_HOST,
+	port: DB_PORT,
+	database: DB_NAME,
+	user: DB_USER,
+	password: DB_PASSWORD
 })
 
 export default pool
