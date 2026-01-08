de5f139c1bdd4fc37b7cd3d9c5ba195362aa6ebe
Jakob Rejdemyhr <jakob.rejdemyhr@gmail.com>
2026-01-08 16:16:53 +0100

    change navbar

--- src/lib/components/AuthButton.svelte
+++ src/lib/components/AuthButton.svelte
@@ -20,23 +20,5 @@
 </button>
 
 <style scoped>
-	button {
-		padding: 0.5rem 1rem;
-		font-size: 1rem;
-		border: none;
-		border-radius: 4px;
-		background-color: #0070f3;
-		color: white;
-		cursor: pointer;
-		transition: background-color 0.3s ease;
-	}
 
-	button:disabled {
-		background-color: #999;
-		cursor: not-allowed;
-	}
-
-	button:hover:enabled {
-		background-color: #005bb5;
-	}
 </style>
